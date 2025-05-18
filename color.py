6487017c2862f7f2f6267381eeddfca313e56cc5
mayihua <mayihua2005@126.com>
2025-05-18 15:49:28 +0800

    文件改动：分离出color.py文件

---
+++ color.py
@@ -0,0 +1,13 @@
+RED = "\033[01;31m"
+GREEN = "\033[01;32m"
+YELLOW = "\033[01;33m"
+BLUE = "\033[01;34m"
+PURPLE = "\033[01;35m"
+CYAN = "\033[01;36m"
+
+
+END = "\033[0m"
+
+
+def color(string: str, color: str):
+    return color + string + END
--- qshifter.py
+++ qshifter.py
@@ -1,24 +1,15 @@
 from qshifter import QuickShifter
-from app import app
+from color import *
 import argparse
 
-RED = "\033[01;31m"
-GREEN = "\033[01;32m"
-YELLOW = "\033[01;33m"
-BLUE = "\033[01;34m"
 
-END = "\033[0m"
-
-
-def color(string: str, color: str):
-    return color + string + END
-
+VERSION = "0.1.0"
 
 BANNER: str = color(
-    """\
+    f"""\
             _     _  ___
            | |   (_)/ __)_
-  ____  ___| | _  _| |__| |_  ____  ____   v 0.1.0
+  ____  ___| | _  _| |__| |_  ____  ____   v {VERSION}
  / _  |/___) || \\| |  __)  _)/ _  )/ ___)
 | | | |___ | | | | | |  | |_( (/ /| |
  \\_|| (___/|_| |_|_|_|   \\___)____)_|
@@ -45,6 +36,8 @@ def interactive() -> bool:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="将输入的字符串按单词循环移位")
+    parser.add_argument("-V", "--version", action="store_true", help="显示版本信息")
+
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-p", "--input", type=str, help="需要循环移位的字符串")
     group.add_argument(
@@ -62,15 +55,23 @@ if __name__ == "__main__":
 
     # TODO: 增加详细信息功能(-v)
 
+    if args.version:
+        print(BANNER)
+        print("Copyright (c) 2025 软件工程小组. All Rights Reserved.")
+        exit(0)
+
     if args.input is not None:
         shifter = QuickShifter(args.input)
         shifter.show()
     elif args.console:
         print(BANNER)
         print(parser.description)
-        print("请按CTRL+C/CTRL+D退出")
+        print(f"请按 {color("CTRL+C/CTRL+D", YELLOW)} 退出")
         while interactive():
             pass
     elif args.server:
-        print("enter server mode")
+        from app import app
+
+        print(BANNER)
+        print("进入服务器模式")
         app.run()
